5bf68efe1edfea209a949b5d97a6ad95558c54f7
Екатерина Краснова <97725329+krasnovaek@users.noreply.github.com>
2024-09-06 12:15:04 +0300

    добавляет функции

---
+++ js/functions.js
@@ -0,0 +1,17 @@
+<script>
+  let calculateSum = function (numberFirst, numberSecond) {
+    let sum = numberFirst + numberSecond;
+  return sum;
+    };
+  let str = 'abcde';
+  console.log(str.length);
+  text = text.replace(/ /g,
+  function() {
+        if (++count > 1) {
+            return '';
+        } else {
+            return ' ';
+        }
+    });
+  console.log(text);
+</script>
