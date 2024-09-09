dbbc69fd9a21b89170b5dae7f9f6b17b29af1aa3
Екатерина Краснова <97725329+krasnovaek@users.noreply.github.com>
2024-09-09 10:34:43 +0300

    добавляет функции

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,30 @@
-<script>
-  let calculateSum = function (numberFirst, numberSecond) {
-    let sum = numberFirst + numberSecond;
-  return sum;
-    };
-  let str = 'abcde';
-  console.log(str.length);
-  text = text.replace(/ /g,
-  function() {
-        if (++count > 1) {
-            return '';
-        } else {
-            return ' ';
-        }
-    });
-  console.log(text);
-</script>
+
+const isPalindrome = (string) => {
+  const normalizedString = string.replaceAll(' ', '').toLowerCase();
+  let reverseString = '';
+
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    reverseString += normalizedString[i];
+  }
+
+  return normalizedString === reverseString;
+};
+
+const checkPalindrome = (string) => {
+  const normalizedString = string.replaceAll(' ', '').toLowerCase();
+  const reverseString = normalizedString.split('').reverse().join('');
+  return normalizedString === reverseString;
+};
+const getNumber = (string) => {
+  const normalizedString = string.toString();
+  let resultNumber = '';
+
+  for (let i = 0; i < normalizedString.length; i++) {
+    const checkedSymbol = parseInt(normalizedString[i], 10);
+    if (!Number.isNaN(checkedSymbol)) {
+      resultNumber += checkedSymbol;
+    }
+  }
+
+  return resultNumber === '' ? NaN : Number(resultNumber);
+};
